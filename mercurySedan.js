9dc0549f5592b5834a7b6f0066aedfd7393cd57a
CycloneSP <glaciersp@gmail.com>
2023-12-02 17:20:08 -0500

    comment

--- mercurySedan.js
+++ mercurySedan.js
@@ -13,23 +13,70 @@ console.log(v.make)
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
 
 
-
-
-
-
-
-
-
-
+}
 
 //TO DO: Creating Instances and Testing Them
 
 //You can use the same instance "v" of the Vehicle class above for the base class.
 
 
-
-
-
 //Create at least two new instances of the Car class and test them here:
+let myCar = new Car('ford', 'fusion', '2007', 'grey', 250000)
+let brandNewCar = new Car('ford', 'fusion', '2023', 'grey', 0)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
+
+brandNewCar.start()
+brandNewCar.loadPassenger(5)
+brandNewCar.stop()
+brandNewCar.checkService()
+
+console.log(brandNewCar)
